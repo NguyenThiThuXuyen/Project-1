9e669c66f81672ecbbc88dca753723134929d9c9
NguyenThiThuXuyen <xu2603@gmail.com>
2019-09-20 21:31:19 +0700

    check value

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import './login.scss';
+import {Button} from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+import {Redirect } from 'react-router-dom'; 
 
 class LoginPage extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className='main'>
-                    <div className='login'>
-                        <div className='circle'>
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
+   constructor(){
+      super()
+      this.state={
+         logined: false
+      }
+   }
+   onClick = (event) => {
+      console.log(event)
+      console.log(event.currentTarget)
+      let cookie = new Cookies()
+      cookie.set('isLogin', 'true')
+      this.setState ( {
+         logined: cookie.get('isLogin') === 'true'
+      })
+   }
+
+   renderDirectTo() {
+      if (this.state.logined) {
+         return <Redirect to = '/home'> </Redirect>
+      }
+   }
+
+   
+   render() {
+      return (
+         <React.Fragment>
+            {this.renderDirectTo()}
+            <div className='main'>
+               <div className='login'>
+                  <div className='circle'>
+                  </div>
+               </div>
+               <div>
+                  <Button onClick={(e) => this.onClick(e)}> Login </Button>
+               </div>
+
+            </div>
+         </React.Fragment>
+      )
+   }
 }
+
 
 export default LoginPage
