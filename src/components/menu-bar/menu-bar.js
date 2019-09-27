404ea0cec76a553b767a802364e3e564cd9ebb32
NguyenThiThuXuyen <xu2603@gmail.com>
2019-09-27 10:27:19 +0700

    locale + listen

--- src/components/menu-bar/menu-bar.js
+++ src/components/menu-bar/menu-bar.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import './menu-bar';
+import '/.menu-bar.scss';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { locale } from '../../config/locale';
+import Cookies from 'universal-cookie';
+import { ListenService } from '../../services/auth/listen';
 
 class MenuBar extends React.Component {
+    constructor() {
+        super()
+
+    }
+    switchLanguage = (lang) => { //truyen bien
+        let cookie = new Cookies()
+        cookie.set('lang', lang)
+        locale.setLanguage(lang) // update locale
+        this.setState({}) // update request
+        ListenService.switchLang(lang)
+    }
     render() {
         return (
             <React.Fragment>
@@ -14,21 +28,26 @@ class MenuBar extends React.Component {
                             <Nav.Link href="/"> Trang Chủ </Nav.Link>
 
                             <NavDropdown title="Khóa Đào Tạo" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Tiếng Hàn</NavDropdown.Item>
+                                <NavDropdown.Item href="#"> Web Design</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Mobile</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#">Fullstack Developer</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/blog">Blog</Nav.Link>
-                            <Nav.Link href="/gioi-thieu"> Giới Thiệu </Nav.Link>
-                            <Nav.Link href="/tuyen-dung"> Tuyển Dụng </Nav.Link>
-                            <Nav.Link href="/lien-he"> Liên Hệ </Nav.Link>
+                            <Nav.Link href="./blog">{locale.blog}</Nav.Link>
+                            <Nav.Link href="./about">{locale.about}</Nav.Link>
+                            <Nav.Link href="./conct">{locale.contact}</Nav.Link>
+                            <Nav.Link href="./tuyen-dung">{locale.hire}</Nav.Link>
                         </Nav>
                         <Form inline>
-                            <FormControl type="text" placeholder="Tìm Kiếm " className="mr-sm-2" />
-                            <Button variant="outline-success">Tiềm Kiếm</Button>
+                            <FormControl type="text" placeholder={locale.search} className="mr-sm-2" />
+                            <Button variant="outline-success">{locale.search}</Button>
                         </Form>
+                        <NavDropdown title={locale.lang} id="basic-nav-dropdown">
+                            <NavDropdown.Item onSelect={() => this.switchLanguage('vi')}>{locale.vi}</NavDropdown.Item>
+                            <NavDropdown.Item onSelect={() => this.switchLanguage('en')}>{locale.en}</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/">{locale.logout}</Nav.Link>
                     </Navbar.Collapse>
                 </Navbar>
 
