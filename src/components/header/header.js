addca912e80b5672e107c36d2241572a7343f8ac
NguyenThiThuXuyen <xu2603@gmail.com>
2019-10-02 21:16:20 +0700

    Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Slide } from 'react-slideshow-image';
+
 import './header.scss';
+
+
+const slideImages = [
+    'https://picsum.photos/id/1040/1600/600',
+    'https://picsum.photos/id/122/1600/600',
+    'https://picsum.photos/id/142/1600/600',
+    'https://picsum.photos/id/420/1600/600',
+    'https://picsum.photos/id/667/1600/600',
+    'https://picsum.photos/id/249/1600/600'
+];
+
+const properties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,  //sau 3 hinh quay lai hinh 1
+    indicators: true, // dau cham hien thi co bn hinh
+    arrows: false, // dau mui ten 2 ben
+    onChange: (oldIndex, newIndex) => {
+        console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+}
 
 class HeaderElement extends React.Component {
     render() {
         return (
-            <div className="app-header">
-
+            <div className="slide-container">
+                <Slide {...properties}>
+                    {
+                        slideImages.map(
+                            (v, i) => (
+                                <div className="each-slide">
+                                    <div style={{ 'backgroundImage': `url(${slideImages[i]})` }}></div>
+                                </div>
+                            )
+                        )
+                    }
+                </Slide>
             </div>
         )
     }
--- src/pages/tuyen-dung/tuyen-dung.js
+++ src/pages/tuyen-dung/tuyen-dung.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './tuyen-dung.scss';
+import HeaderElement from '../../components/header/header';
 
 class TuyenDung extends React.Component {
     render() {     
         return (
             <React.Fragment>
                 <h1>tuyen dung</h1>
+                <HeaderElement></HeaderElement>
             </React.Fragment>
 
         )
